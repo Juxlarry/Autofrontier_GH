a7a700447d73eb60d3300f7b5ed38851c5370dfc
Lawrence D <60719231+Juxlarry@users.noreply.github.com>
2021-05-06 15:47:52 -0700

    Enquiries Functionality Working

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -32,7 +32,11 @@ class CarsController < ApplicationController
 
   # GET /cars/1 or /cars/1.json
   def show
-    #@car = Car.with_attached_images.find(params[:id])
+
+    @car_on_show = Car.where(id: params[:id])
+
+    @car_poster_id = @car_on_show.car_user_id
+
     @car = Car.joins("
       INNER JOIN car_types ON car_types.id = cars.car_type_id
       INNER JOIN fuel_types ON fuel_types.id = cars.fuel_type_id
@@ -46,8 +50,9 @@ class CarsController < ApplicationController
       INNER JOIN car_documents ON car_documents.id = cars.car_doc_id
       INNER JOIN car_engines ON car_engines.id = cars.car_engine_id
       INNER JOIN vehicle_types ON vehicle_types.id = cars.vehicle_type_id
+      INNER JOIN users ON users.id = cars.car_user_id
       ").select(
-      " cars.id, car_types.id as car_type_id, car_types.name as car_type, car_name, car_price, 
+      " cars.id, car_types.id as car_type_id, car_types.name as car_type, car_name, car_price,
         mileage as car_mileage, car_model_year, cars.description as car_description, car_registration_number,
         car_registration_first_date, trim_details, derivative_details, cars.status as car_status,
         variant_details, cars.seats as car_seat, cars.doors as car_door, cars.comments as comment_on_car,
@@ -55,9 +60,15 @@ class CarsController < ApplicationController
         fuel_types.fuel_type as car_fuel, car_colours.name as car_colour, car_bodies.type_name as car_body,
         car_transmissions.name as car_transmission, car_makes.name as car_make, car_models.name as car_model,
         car_drives.drive_type as car_drive, car_cylinders.cylinder as car_cylinder, car_documents.doc_type as car_doc,
-        car_documents.doc_valid as car_doc_valid, car_engines.engine_type as car_engine, vehicle_types.vehicle as vehicle_type
+        car_documents.doc_valid as car_doc_valid, car_engines.engine_type as car_engine, vehicle_types.vehicle as vehicle_type, 
+        users.email as car_seller_email, users.username as car_seller_username
       "
     ).with_attached_images.find(params[:id])
+
+    logger.info "car on show details ::: #{@car_on_show.inspect}"
+
+
+    logger.info "car on show user id ::: #{@car_poster_id}"
 
     #calculating price to pay of there is discount price from seller noted
     #this is noted as the {estimated_price}
@@ -83,23 +94,9 @@ class CarsController < ApplicationController
 
     @enquiry_request = Vehicleenquiry.new
 
-    
-  end
 
-  # GET /cars/new
-  def new
-    @car = Car.new
-  end
-
-  # Make enquiry to create an enquiry for a car
-  def make_enquiry
-    @enquiry_request = Vehicleenquiry.new
-
-    @car_details_id = params[:id]
-
-    logger.info " car id is ----- #{@car_details_id}"
-
-    # @car_details = Car.joins("
+    #Cars by seller 
+    # @car_by_seller = Car.joins("
     #   INNER JOIN car_types ON car_types.id = cars.car_type_id
     #   INNER JOIN fuel_types ON fuel_types.id = cars.fuel_type_id
     #   INNER JOIN car_colours ON car_colours.id = cars.colour_id
@@ -125,7 +122,50 @@ class CarsController < ApplicationController
     #     car_documents.doc_valid as car_doc_valid, car_engines.engine_type as car_engine, vehicle_types.vehicle as vehicle_type, 
     #     users.email as car_seller_email, users.username as car_seller_username
     #   "
-    # ).where(:id => @car_details_id)
+    # ).where(car_user_id: )
+    
+  end
+
+  # GET /cars/new
+  def new
+    @car = Car.new
+  end
+
+  # Make enquiry to create an enquiry for a car
+  def make_enquiry
+    @enquiry_request = Vehicleenquiry.new
+
+    @car_details_id = params[:id]
+
+    logger.info " car id is ----- #{@car_details_id}"
+
+    @car_details = Car.joins("
+      INNER JOIN car_types ON car_types.id = cars.car_type_id
+      INNER JOIN fuel_types ON fuel_types.id = cars.fuel_type_id
+      INNER JOIN car_colours ON car_colours.id = cars.colour_id
+      INNER JOIN car_bodies ON car_bodies.id = cars.body_type_id
+      INNER JOIN car_transmissions ON car_transmissions.id = cars.transmission_type_id
+      INNER JOIN car_makes ON car_makes.id = cars.make_id
+      INNER JOIN car_models ON car_models.id = cars.model_id
+      INNER JOIN car_drives ON car_drives.id = cars.drive_type
+      INNER JOIN car_cylinders ON car_cylinders.id = cars.car_cylinder
+      INNER JOIN car_documents ON car_documents.id = cars.car_doc_id
+      INNER JOIN car_engines ON car_engines.id = cars.car_engine_id
+      INNER JOIN vehicle_types ON vehicle_types.id = cars.vehicle_type_id
+      INNER JOIN users ON users.id = cars.car_user_id
+      ").select(
+      " cars.id, car_types.id as car_type_id, car_types.name as car_type, car_name, car_price,
+        mileage as car_mileage, car_model_year, cars.description as car_description, car_registration_number,
+        car_registration_first_date, trim_details, derivative_details, cars.status as car_status,
+        variant_details, cars.seats as car_seat, cars.doors as car_door, cars.comments as comment_on_car,
+        car_keys, car_primary_damage, car_secondary_damage, estimated_retail_price, car_highlights, 
+        fuel_types.fuel_type as car_fuel, car_colours.name as car_colour, car_bodies.type_name as car_body,
+        car_transmissions.name as car_transmission, car_makes.name as car_make, car_models.name as car_model,
+        car_drives.drive_type as car_drive, car_cylinders.cylinder as car_cylinder, car_documents.doc_type as car_doc,
+        car_documents.doc_valid as car_doc_valid, car_engines.engine_type as car_engine, vehicle_types.vehicle as vehicle_type, 
+        users.email as car_seller_email, users.username as car_seller_username
+      "
+    ).where(:id => @car_details_id)
     
     
     
