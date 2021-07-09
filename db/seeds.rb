673672ebf8f227b36a83c335611d9473727df052
Lawrence D <juxlarry@gmail.com>
2021-07-09 12:45:53 -0700

    Added Seed for Roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+roles = Role.create([{role_name: 'Admin'}, {role_name: 'Vendor'}, {role_name: 'Buyer'}])
